4925245e37d1f42c28b34d4d4aed4a1b1b94aa01
Dev Shah <dev1.mitmpl2024@learner.manipal.edu>
2025-12-26 23:48:13 +0530

    Delete hooks directory

--- hooks/useServerDarkMode.js
+++
@@ -1,7 +0,0 @@
-import {cookies} from "next/headers";
-
-const useServerDarkMode = (defaultTheme = 'light') => {
-    return cookies().get('theme')?.value ?? defaultTheme;
-}
-
-export default useServerDarkMode;
