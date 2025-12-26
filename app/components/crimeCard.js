922947d2887982810eb4d426dc47f2f3e8b509e5
Dev Shah <dev1.mitmpl2024@learner.manipal.edu>
2025-12-26 23:47:38 +0530

    Delete app directory

--- app/components/crimeCard.js
+++
@@ -1,7 +0,0 @@
-export default function CrimeCard() {
-    return (
-        <div className="flex justify-between">
-            hello
-        </div>
-    )
-}
--- app/components/label.js
+++
@@ -1,5 +0,0 @@
-export default function Label(props) {
-    return (
-        <label {...props} className={`block mb-1 text-gray-700 dark:text-gray-300 ${props.className}`}></label>
-    )
-}
